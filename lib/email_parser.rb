16e26d1b7011d979d174eaffb3d65b95fb8cd9b9
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-12 07:25:30 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailParser
   
   def parse
     # binding.pry
-    return @emails.split(/[\s,]/).reject!{|item| item.empty?}.uniq!
+    return @emails.split(/[\s,]/).reject!{|item| item.empty?}
   end
 end
 
